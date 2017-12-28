0eef119a3f4119cbcc7d59699019146f8acd2f0e
Jason Vance <jbvance@gmail.com>
2017-12-28 08:53:22 -0600

    add react router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// BrowserRouter interacts with the history and decides what
+// to do based on a change on the URL
+// Route provides configuration to say what component to render
+// based on what the url looks like
+import { BrowserRouter, Route} from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+   <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />        
+      </div>
+   </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
