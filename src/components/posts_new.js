a653be75e7056f35e010f999df5f3237ac8d938d
Jason Vance <jbvance@gmail.com>
2017-12-28 14:53:52 -0600

    add fields to new posts form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,10 +3,12 @@ import { Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
 
-    renderTitleField(field) {
+    renderField(field) {
         return (
-            <div>
+            <div className="form-group">
+                <label>{field.label}</label>
                 <input 
+                    className="form-control"
                     type="text"
                     {...field.input}
                 />
@@ -14,12 +16,24 @@ class PostsNew extends Component {
         );
     }
 
+
     render() {
         return (
             <form>
                 <Field 
+                    label="Title"
                     name="title"
-                    component={this.renderTitleField}
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
                 />
             </form>
         );
