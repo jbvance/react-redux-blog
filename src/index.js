9be8fa81a8d75e8a7597ede37706443e1dafbf46
Jason Vance <jbvance@gmail.com>
2017-12-28 14:06:08 -0600

    update routing and begin new post component

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import promise from 'redux-promise'
 // to do based on a change on the URL
 // Route provides configuration to say what component to render
 // based on what the url looks like
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -19,7 +20,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
    <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />        
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} /> 
+          <Route path="/" component={PostsIndex} />         
+        </Switch>    
       </div>
    </BrowserRouter>
   </Provider>
