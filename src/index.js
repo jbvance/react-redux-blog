e62e57fbfacc7f80e3767e239a9954098ff48783
Jason Vance <jbvance@gmail.com>
2017-12-29 10:09:07 -0600

    complete show post functionality

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -22,6 +23,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} /> 
+          <Route path="/posts/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} />         
         </Switch>    
       </div>
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,22 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 export default function(state = {}, action) {
     switch (action.type) {
+        case FETCH_POST:
+            const post = action.payload.data;
+            const newState = { ...state }
+            newState[post.id] = post;
+            return newState;
+            // can shorten above to one line as follows:
+            //return {...state, [action.payload.data.id]: action.payload.data};
+          
         case FETCH_POSTS:
             //console.log(action.payload.data); // array of post objects from api
-            return _.mapKeys(action.payload.data, 'id');
+            return _.mapKeys(action.payload.data, 'id');        
 
         default:
             return state;
-    }
+    }       
+
 }
